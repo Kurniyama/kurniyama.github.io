65fb43b5e952c508a488f1657a3c2f16be6181ed
Kurniyama <akbarkurniaaidil@gmail.com>
2024-12-27 18:08:49 +0700

    Update happy birtday.js

--- happy birtday.js
+++ happy birtday.js
@@ -5,7 +5,7 @@ let w = (c.width = window.innerWidth),
 (hh = h / 2),
   (opts = {
     // change the text in here //
-    strings: ["SELAMAT", "ULANG TAHUN", "KANTIKA"],
+    strings: ["HAPPY", "BIRTHDAY", "KANTIKA"],
     charSize: 30,
     charSpacing: 35,
     lineHeight: 40,
